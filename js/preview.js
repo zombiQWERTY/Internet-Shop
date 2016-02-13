07817c016454d883a3fbef4829ae9603fe6f3a15
Kate <ekaterina_kashchenko@yahoo.com>
2016-02-13 21:03:28 +0200

    try to get object by id string

--- js/preview.js
+++ js/preview.js
@@ -1,4 +1,98 @@
-;
-window.onload = function(){
-	
-}
+$(function() {
+	if (window.location.pathname.indexOf('pdp') + 1) {
+		$.ajax({
+				dataType: 'json',
+				url: '../json/clothes.json',
+				success: function(data) {
+						var goods_key = Object.keys(data);
+						console.log('Connect');
+						var my_hash = location.hash; 
+						var myId    = my_hash.substr(my_hash.indexOf("#") + 1); 
+						console.log(myId);
+						
+						function getById(myId) {
+							console.log(myId);
+							//console.log(data[goods_key[2]][3].id)
+							for (var i = 0; i <= 2; i++) {
+								for (var j = 0; j <= 3; j++) {
+									if (myId = data[goods_key[i]][j].id) {
+										// console.log(data[goods_key[i]][j].id)
+										return data[goods_key[i]][j].id;
+									} else {
+										console.log('jjjjjjjj');
+									}
+								};
+								console.log(data[goods_key[i]][j].id)
+							};
+							
+						}
+						getById(myId)
+					//	console.log(data[goods_key[2]][3].id);
+
+
+						function building_gallery () {
+							var divGoodsGallery = document.getElementsByClassName('goods__gallery')[0];
+							var divGoodsGalleryMain = document.createElement('div');
+									divGoodsGalleryMain.className = 'goods__gallery-main';
+									divGoodsGallery.appendChild(divGoodsGalleryMain);
+							var imgGoodsGallery = document.createElement('img');
+
+									imgGoodsGallery.src = '../img/men-clother/t-shirts/1.jpg'; //from ajax
+									imgGoodsGallery.alt = 'pic';
+									divGoodsGalleryMain.appendChild(imgGoodsGallery);
+							var divGoodsGalleryWrapper = document.createElement('div');
+									divGoodsGalleryWrapper.className = 'goods__gallery_wrapper';
+									divGoodsGallery.appendChild(divGoodsGalleryWrapper);
+
+									for (var i = 0; i <= goods_key.length-1; i++) {
+										var divGoodsGalleryWrapperItem = document.createElement('div');
+												divGoodsGalleryWrapperItem.className = 'goods__gallery_wrapper-item';
+												divGoodsGalleryWrapper.appendChild(divGoodsGalleryWrapperItem);
+										var imgGoodsGalleryPreview = document.createElement('img');
+												imgGoodsGalleryPreview.src = '../img/men-clother/t-shirts/1.jpg'; //from ajax
+												imgGoodsGalleryPreview.alt = 'pic';
+												divGoodsGalleryWrapperItem.appendChild(imgGoodsGalleryPreview);
+									};
+
+							var divGoodsWrapperContent = document.getElementsByClassName('goods__wrapper__content')[0];
+							var h2Header = document.createElement('h2');
+									h2Header.innerHTML = 'take from ajax';
+									divGoodsWrapperContent.appendChild(h2Header);
+							var pSubheaderSmall = document.createElement('p');
+									pSubheaderSmall.className = 'subheader_small';
+									pSubheaderSmall.innerHTML = 'Article number: ' + 'take from ajax';
+									divGoodsWrapperContent.appendChild(pSubheaderSmall);
+							var pGoodsPrice = document.createElement('p');
+									pGoodsPrice.className = 'goods__wrapper__price';
+									pGoodsPrice.innerHTML = '&euro; ' + 'take from ajax';
+									divGoodsWrapperContent.appendChild(pGoodsPrice);
+							var pGoodsDescription = document.createElement('p');
+									pGoodsDescription.className = 'goods__wrapper__description';
+									pGoodsDescription.innerHTML = 'take from ajax';
+									divGoodsWrapperContent.appendChild(pGoodsDescription);
+							var pGoodsSize = document.createElement('p');
+									pGoodsSize.className = 'goods__wrapper__size';
+									pGoodsSize.innerHTML = 'Size';
+									divGoodsWrapperContent.appendChild(pGoodsSize);
+							var divGoodsSizeChoose = document.createElement('div');
+									divGoodsSizeChoose.className = 'goods__wrapper__size';
+									divGoodsWrapperContent.appendChild(divGoodsSizeChoose);
+							//взять в цикл
+							var divSize = document.createElement('div');
+									divSize.className = 'size';
+									divSize.innerHTML = 'ajax';
+									divGoodsSizeChoose.appendChild(divSize);
+							// end cukl
+
+
+
+						}
+building_gallery();
+
+				},
+				error: function(data) {
+						console.log('Connect error');
+				}
+		});
+	}
+});
